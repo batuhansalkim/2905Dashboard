97baf81d43ccd608b23556420169c611a9eccab6
batuhan <batusalkim11@gmail.com>
2023-05-30 14:39:56 +0300

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <AppHeader/>
-      <Space>
+      <Space className="SideMenuAndPageContent">
         <SideMenu></SideMenu>
         <PageContent></PageContent>
       </Space>
--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,7 +1,11 @@
+import { Typography } from "antd";
 function AppFooter(){
     return(
         <div className="AppFooter">
-        appfooter
+            <Typography.Link href="tel:+123456789">+123456789</Typography.Link>
+            <Typography.Link>Linkedin</Typography.Link>
+            <Typography.Link>Instagram</Typography.Link>
+            <Typography.Link>Github</Typography.Link>
         </div>
     )
 }
