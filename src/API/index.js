5013651051111ff0041da3549b791ea3a6e106c4
batuhan <batusalkim11@gmail.com>
2023-06-01 02:28:44 +0300

    update

--- src/API/index.js
+++ src/API/index.js
@@ -1,3 +1,20 @@
 export const getOrders =()=>{
     return fetch('https://dummyjson.com/carts/1').then(res => res.json())
 }
+
+export const getRevenue=()=>{
+    return fetch('https://dummyjson.com/carts').then(res => res.json())
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,13 +2,31 @@ import { ShoppingCartOutlined, ShoppingOutlined,UserOutlined,DollarCircleOutline
 import { Typography,Card,Space,Statistic, Table } from "antd";
 import { useEffect, useState } from "react";
 import { getOrders } from "../../API";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 function Dashboard() {
     return (
-        <div>
+        <Space size={20} direction="vertical">
             <Typography.Title level={4}>Dashboard</Typography.Title>
-            <Space direction="horizontal">
+            <Space direction="horizontal" >
                 <DashboardCard icon={<ShoppingCartOutlined style={{color:"green", backgroundColor:"rgba(0,255,0,0.25)",borderRadius:20, fontSize:24,padding:8}}/>} title={"Orders"} value={12345}/>
 
                 <DashboardCard icon={<ShoppingOutlined style={{ color: "blue", backgroundColor: "rgba(0,0,255,0.25)", borderRadius: 20, fontSize: 24, padding: 8 }} />} title={"Inventory"} value={105}/>
@@ -16,11 +34,15 @@ function Dashboard() {
                 <DashboardCard icon={<UserOutlined style={{ color: "purple", backgroundColor: "rgba(0,255,255,0.25)", borderRadius: 20, fontSize: 24, padding: 8 }} />} title={"Customer"} value={120}/>
 
                 <DashboardCard icon={<DollarCircleOutlined style={{ color: "red", backgroundColor: "rgba(255,0,0,0.25)", borderRadius: 20, fontSize: 24, padding: 8 }} />} title={"Revenue"} value={1246}/>
+                
             </Space>
-            <Space>
-                <RecentOrders/>
+            
+            <Space >
+                <RecentOrders  />
+                <DashboardCart/>
             </Space>
-        </div>
+            
+        </Space>
     )
 }
 
@@ -40,12 +62,16 @@ function RecentOrders(){
 
     useEffect(()=>{
         setLoading(true);
-        getOrders().then(res=>{
-            setDataSource(res.products);
-            setLoading(false);   
+        getOrders().then((res)=>{
+            console.log(res.products);
+            setDataSource(res.products.splice(0,3))
+            setLoading(false);
+             
         });
     },[])
     return(
+        <>
+        <Typography.Title level={4}>Recent Orders</Typography.Title>
         <Table columns={[
             {
                 title:"Title",
@@ -58,10 +84,51 @@ function RecentOrders(){
             {
                 title:"Price",
                 dataIndex:"discountedPrice"
+            },
+            {
+                title: "Discount rate",
+                dataIndex:"discountPercentage"
             }
-        ]}>
-
+        ]}
+            loading={loading}
+            dataSource={dataSource}
+            pagination={false}
+        >
+            
         </Table>
+        </>
     )
+}
+function DashboardCart(){
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom' ,
+            },
+            title: {
+                display: true,
+                text: 'Order Revenue',
+            },
+        },
+    };
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: labels.map(() => Math.random()*1000),
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Dataset 2',
+                data: labels.map(() => Math.random()*1000),
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    };
+    return <Bar options={options} data={data}/>;
 }
 export default Dashboard;
